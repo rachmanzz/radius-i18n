8c1b7f27a7764c8674f9d61a4693ce9e0ad8eb8c
Muhammad Abdurrahman <rachman.sd@gmail.com>
2024-11-30 22:53:36 +0700

    build: update rollup config

--- packages/radius-i18n/rollup.config.mjs
+++ packages/radius-i18n/rollup.config.mjs
@@ -1,23 +1,32 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
+import  terser  from "@rollup/plugin-terser";
+import copy from 'rollup-plugin-copy'
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json'; 
 
 export default {
-  input: 'src/bin/index.ts',
+  input: 'src/bin/index.ts', // Specify the input file here
   output: [
     {
       dir: 'dist',  // Output directory
       format: 'esm',  // ES Module format
       preserveModules: true,  // Keep modules separated
       preserveModulesRoot: 'src',  // Maintain the structure
-    },
-    {
-      file: 'dist/bin/index.mjs',  // Output bin file as .mjs
-      format: 'esm',  // ES module format
-      exports: 'default',  // Export default from the bin file
-    },
+      entryFileNames: '[name].mjs',  // Add this line to ensure the file extension is .mjs
+    }
   ],
+  external: ['google-auth-library', 'navigator', 'window', 'document'],
   plugins: [
-    resolve(),  // Resolves external dependencies
-    typescript({ tsconfig: './tsconfig.json' }),  // TypeScript plugin
+    commonjs(),
+    resolve({preferBuiltins: true, browser: false}),  // Resolves external dependencies
+    typescript({ tsconfig: './tsconfig.json' }),  // TypeScript plugin,
+    json(), 
+    terser(),
+    copy({
+      targets: [
+        { src: "src/instructions/*", dest: "dist/instructions" }
+      ]
+    })
   ],
 };
