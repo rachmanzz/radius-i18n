6511fe4d439555e5770b01b9ce6d309e3b34082a
Muhammad Abdurrahman <rachman.sd@gmail.com>
2024-11-30 23:00:09 +0700

    feat: bin index, map config to env

--- packages/radius-i18n/src/bin/index.ts
+++ packages/radius-i18n/src/bin/index.ts
@@ -1,5 +1,28 @@
-export default function Translate() {
+import { readFileJSON } from "../helper/file";
+import { logError } from "../helper/logger";
+import { waitForInput } from "../input-handler";
+import watcher from "../watcher";
 
+const startRadius = async () => {
+    try {
+        const config = await readFileJSON("./config.radius.json")
+        for (let key in config) {
+            if (key === "service_resource") {
+                process.env["GOOGLE_APPLICATION_CREDENTIALS"] = config[key]
+                continue
+            }
+            if (["locale_base", "locales", "locale_path", "change_delay", "freeze_in", "wait_for_freeze", "gemini_model"].indexOf(key) >= 0) {
+                const ENV_KEY = key.toUpperCase();
+                process.env["RADIUS_" + ENV_KEY] = Array.isArray(config[key]) ? config[key].join(",") : config[key];
+            }
+        }
+        await watcher()
+    } catch (error) {
+        logError("message" in (error as any) ? (error as any).message : "unknown error message" )
+    }
+}
 
-    console.log("helo world")
-} 
+startRadius()
+waitForInput()
+
+export default startRadius;
